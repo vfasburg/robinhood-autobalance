58c8d1b1296d06e30e095fb184e64c521c5f4a9d
Vince Fasburg <vfasburg@umich.edu>
2018-04-09 02:39:56 +0000

    added calculation of shares to be bought

--- app.js
+++ app.js
@@ -5,30 +5,50 @@ var _ = require('lodash');
 var fs = require('fs');
 
 var accounts = require(__dirname + '/accounts.json');
-var plans = require(__dirname + '/portfolios.json');
+var portfolioPlans = require(__dirname + '/portfolios.json');
 
 
-function calculateOrders(Robinhood, portfolio) {
-  return null;
+function calculateOrders(currentStocks, portfolioPlan, cash) {
+  var portfolioStocks = _.pick(currentStocks, _.keys(portfolioPlan));
+  var portfolioValue = 0;
+  _.each(portfolioStocks, function(stock) {
+    stock.value = stock.price * stock.shares;
+    portfolioValue += stock.value;
+  })
+  var newPortfolioValue = portfolioValue + cash;
+  _.each(portfolioStocks, function(stock, symbol) {
+    stock.ideal_value = newPortfolioValue * portfolioPlan[symbol];
+    stock.ideal_shares = stock.ideal_value / stock.price;
+    stock.balanced_shares = Math.floor(stock.ideal_shares);
+    stock.shares_to_buy = stock.balanced_shares - stock.shares;
+  })
+  console.log(portfolioStocks);
 }
 
 
-function login() {
-  var username = accounts[0].username;
-  var passwordB64 = accounts[0].password;
-  var passwordClear = Buffer.from(passwordB64, 'base64').toString();
+function login(username, password) {
   var retval = new Promise(function(resolve, reject) {
-    var client = require('robinhood')({username: username, password: passwordClear}, function(){
+    var client = require('robinhood')({username: username, password: password}, function(){
       client.accounts(function(err, response, body){
-        if(err){
+        if(err) {
           reject(err);
-        }else{
+        } else {
           resolve(Promise.promisifyAll(client));
         }
       })
     });
   });
   return retval;
+}
+
+function getAvailableCash(Robinhood) {
+  return Robinhood.accountsAsync()
+  .then(resp => {
+    return resp.body.results[0].cash * 1;
+  }, err => {
+    console.log('Error getting account info');
+    console.log(err);
+  })
 }
 
 function getPortfolio(Robinhood) {
@@ -59,8 +79,16 @@ function getPortfolio(Robinhood) {
   })
 }
 
-login()
-.then(getPortfolio)
-.then(portfolio => {
-  console.log(portfolio);
-})
+function start() {
+  _.each(accounts, act => {
+    var passwordClear = Buffer.from(act.password, 'base64').toString();
+    login(act.username, passwordClear).then(Robinhood => {
+      return Promise.all([getPortfolio(Robinhood), getAvailableCash(Robinhood)])
+      .spread((portfolio, cash) => {
+        var portfolioPlan = portfolioPlans[act.username];
+        var orders = calculateOrders(portfolio, portfolioPlan, cash);
+      })
+    })
+  })
+}
+start();
